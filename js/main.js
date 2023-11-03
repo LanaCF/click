06d521ec7dcea9282f1faa8cd8b778d87ac21a7a
Uliana Boiko <clubfate@gmail.com>
2023-11-03 21:36:44 +0200

    HW

---
+++ js/main.js
@@ -0,0 +1,97 @@
+const doc = document;
+const box = doc.querySelector('.box');
+const btnAdd = doc.querySelector('.add');
+const btnDel = doc.querySelector('.del');
+const btnCloneB = doc.querySelector('.before');
+const btnCloneA = doc.querySelector('.after');
+const btn = doc.querySelectorAll('.btn');
+
+let count = 1;
+let element;
+
+btn.forEach(function(btnEl) {
+  btnEl.style.cursor = 'pointer';
+});
+
+btnAdd.onclick = function() {
+  const item = doc.createElement('div');
+  item.className = 'item';
+  item.innerHTML = count;
+
+  item.onclick = function(){
+    unActiveAll();
+    this.classList.toggle('active');
+
+    showCurrentEl(this);
+
+    element = this;
+  };
+
+  count ++;
+  box.append(item);
+}
+
+btnDel.ong = function(){
+  if(element){
+    element.remove();
+  }
+}
+
+btnCloneB.onclick = function() {
+  if(element){
+    let clone = element.cloneNode(true);
+    element.after(clone);
+    element.innerHTML = element.innerHTML + '-c';
+  }
+}
+
+btnCloneA.onclick = function() {
+  if(element){
+    let clone = element.cloneNode(true);
+    element.before(clone);
+    element.innerHTML = element.innerHTML + '-c';
+  }
+}
+
+
+
+
+
+
+
+
+function unActiveAll() {
+  const items = doc.querySelectorAll('.item');
+  items.forEach(function(item){
+    item.classList.remove('active');
+  });
+}
+
+function showCurrentEl(currentEl) {
+  const showEl = doc.querySelector('.selected span:last-child');
+  showEl.innerHTML = currentEl.innerHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+// btnAdd.onclick = function() {
+//   const item = doc.createElement('div');
+//   item.className = 'item';
+//   item.innerHTML = count;
+
+//   item.onclick = function(){
+//     this.remove();
+//   };
+
+//   count ++;
+
+//   box.append(item);
+// }
